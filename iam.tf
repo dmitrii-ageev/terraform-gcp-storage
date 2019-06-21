9dc3e756526089815445ff5324730cd8176a1353
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-06-21 19:52:39 +1200

    Change formatting

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket_iam_member" "this" {
-  count  = "${var.iam_member == "" ? 0 : 1}"
+  count = "${var.iam_member == "" ? 0 : 1}"
 
   bucket = "${local.bucket_name}"
   role   = "roles/storage.objectAdmin"
