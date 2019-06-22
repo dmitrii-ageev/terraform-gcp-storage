6e9d27dd3693f6bb4251186431d025270a691e96
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-06-22 22:04:38 +1200

    Upgrade syntax to Terraform 0.12

--- iam.tf
+++ iam.tf
@@ -1,17 +1,18 @@
 // Reset existing bucket permissions
 resource "google_storage_bucket_iam_binding" "this" {
-  count   = "${var.iam_owner == "" ? 0 : 1}"
-  bucket  = "${local.bucket_name}"
+  count   = var.iam_owner == "" ? 0 : 1
+  bucket  = local.bucket_name
   role    = "roles/storage.legacyBucketOwner"
-  members = ["${var.iam_owner}"]
+  members = [var.iam_owner]
 }
 
 // Grant the storage admin role for the iam_member
 resource "google_storage_bucket_iam_member" "this" {
-  count      = "${var.iam_member == "" ? 0 : 1}"
-  depends_on = ["google_storage_bucket_iam_binding.this"]
+  count      = var.iam_member == "" ? 0 : 1
+  depends_on = [google_storage_bucket_iam_binding.this]
 
-  bucket = "${local.bucket_name}"
+  bucket = local.bucket_name
   role   = "roles/storage.objectAdmin"
-  member = "${var.iam_member}"
+  member = var.iam_member
 }
+
