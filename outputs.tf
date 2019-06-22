8c895b7a2651e40fa775fb762ef0e043967c929d
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-06-22 21:36:00 +1200

    Terraform 0.12 formatting

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,15 @@
 output "url" {
   description = "The base URL of the bucket, in the format gs://<bucket-name>."
-  value       = "${local.bucket_url}"
+  value       = local.bucket_url
 }
 
 output "name" {
   description = "The name of the created bucket"
-  value       = "${local.bucket_name}"
+  value       = local.bucket_name
 }
 
 output "self_link" {
   description = "The URI of the created resource."
-  value       = "${local.bucket_self_link}"
+  value       = local.bucket_self_link
 }
+
