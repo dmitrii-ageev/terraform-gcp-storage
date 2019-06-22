a3d91428394e81792d0645cf297ad58a3605d615
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-06-22 22:45:32 +1200

    Upgrade to Terraform 0.12

--- locals.tf
+++ locals.tf
@@ -1,9 +1,7 @@
 locals {
-  create_default_bucket = var.log_bucket == "" ? 1 : 0
-  create_logging_bucket = 1 - local.create_default_bucket
+  create_default_bucket = ! var.log_bucket
+  create_logging_bucket = var.log_bucket
 
-  bucket_url       = local.create_default_bucket ? google_storage_bucket.default.*.url : google_storage_bucket.logging.*.url
-  bucket_name      = local.create_default_bucket ? google_storage_bucket.default.*.name : google_storage_bucket.logging.*.name
-  bucket_self_link = local.create_default_bucket ? google_storage_bucket.default.*.self_link : google_storage_bucket.logging.*.self_link
+  bucket = local.create_default_bucket ? google_storage_bucket.default : google_storage_bucket.logging
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,5 @@
-output "url" {
-  description = "The base URL of the bucket, in the format gs://<bucket-name>."
-  value       = local.bucket_url
-}
-
-output "name" {
-  description = "The name of the created bucket"
-  value       = local.bucket_name
-}
-
-output "self_link" {
-  description = "The URI of the created resource."
-  value       = local.bucket_self_link
+output "bucket" {
+  description = "The created bucket resource"
+  value       = local.bucket
 }
 
