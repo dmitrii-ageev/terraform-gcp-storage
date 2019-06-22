8f2f79990702887f0b520b031cbb85e5a62ddbbb
Dmitrii Ageev <dmitrii.ageev@unimarket.com>
2019-06-22 22:04:47 +1200

    Upgrade syntax to Terraform 0.12

--- locals.tf
+++ locals.tf
@@ -1,16 +1,9 @@
 locals {
-  create_logging_bucket = "${var.log_bucket == "" ? 0 : 1}"
-  create_default_bucket = "${1 - local.create_logging_bucket}"
+  create_default_bucket = var.log_bucket == "" ? 1 : 0
+  create_logging_bucket = 1 - local.create_default_bucket
 
-  bucket_url = "${join("", concat(
-                                     google_storage_bucket.default.*.url,
-                                     google_storage_bucket.logging.*.url) )}"
-
-  bucket_name = "${join("", concat(
-                                     google_storage_bucket.default.*.name,
-                                     google_storage_bucket.logging.*.name) )}"
-
-  bucket_self_link = "${join("", concat(
-                                     google_storage_bucket.default.*.self_link,
-                                     google_storage_bucket.logging.*.self_link) )}"
+  bucket_url       = local.create_default_bucket ? google_storage_bucket.default.*.url : google_storage_bucket.logging.*.url
+  bucket_name      = local.create_default_bucket ? google_storage_bucket.default.*.name : google_storage_bucket.logging.*.name
+  bucket_self_link = local.create_default_bucket ? google_storage_bucket.default.*.self_link : google_storage_bucket.logging.*.self_link
 }
+
